856e16462e8e7b9aadd4b818396fab724874f1ab
PhillipKregg <phillipkregg@gmail.com>
2012-04-06 11:47:44 -0500

    Initial commit - using Twitter Bootstrap, added KendoUI controls, setup Devise authentication, created Job model, controller and views.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KendoUITest::Application.config.secret_token = '01d95325ac1c9f072a1f46aea2efbe23b636311c89a29d70f04efa57dfa19be589ad159ceaf034860bd9bdcdb7146d6347493fbf37ab5ce0288073f94aac56cb'
---
+++ db/migrate/20120406164608_create_jobs.rb
@@ -0,0 +1,21 @@
+class CreateJobs < ActiveRecord::Migration
+  def change
+    create_table :jobs do |t|
+      t.string :last_name
+      t.string :first_name
+      t.string :loss_city
+      t.string :job_type
+      t.string :insurance_company
+      t.string :age
+      t.datetime :loss_date
+      t.string :job_number
+      t.string :job_status
+      t.string :dispatch_status
+      t.string :caller
+      t.string :occupant
+      t.string :production_manager
+
+      t.timestamps
+    end
+  end
+end
